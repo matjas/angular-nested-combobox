4db9d821be2aacd2b51ee2da6d716c6c52b58fbe
matjas <mjaskula@windowslive.com>
2014-08-15 20:57:45 +0200

    jsLint changes

--- src/ng-nested-combobox.js
+++ src/ng-nested-combobox.js
@@ -2,61 +2,61 @@
  * Created by Maciek on 2014-06-29.
  */
 
-angular.module('ng.nested.combobox', [])
+angular.module('ui.nested.combobox', [])
+    .controller('NestedComboboxController', ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+        'use strict';
+        var that = this,
+            oldMemberId = null;
+        this.isOpen = false;
+        this.currentMember = $scope.currentMember;
+
+        $scope.$watch('controlDisabled', function (value) {
+            that.controlDisabled = value;
+        });
+
+        /* $element.on('blur', function (e) {
+         //that.isOpen.status = !that.isOpen.status;
+         that.isOpen = false;
+         });
+         $element.on('focus', function (e) {
+         //that.isOpen.status = !that.isOpen.status;
+         that.isOpen = true;
+         });*/
+
+        this.toggleOpen = function () {
+
+            if (that.controlDisabled === 'true') {
+                this.isOpen.status = false;
+                return false;
+            }
+            this.isOpen = !this.isOpen;
+        };
+
+        this.selectValue = function (event, member) {
+
+            if (oldMemberId === member.id) {
+                return true;
+            }
+
+            if (member.id === 'root') {
+                member.name = event.currentTarget.innerText;
+            }
+            //that.currentMember = member;
+            $scope.changeEvent(member);
+            that.currentMember = member;
+            oldMemberId = member.id;
+
+        };
+    }])
     .directive('nestedComboBox', function () {
         'use strict';
 
-        var controller = function ($scope, $element, $attrs) {
-            var that = this,
-                oldMemberId = null;
-            this.isOpen = false;
-            this.currentMember = $scope.currentMember;
-
-            $scope.$watch('controlDisabled', function (value) {
-                that.controlDisabled = value;
-            });
-
-           /* $element.on('blur', function (e) {
-                //that.isOpen.status = !that.isOpen.status;
-                that.isOpen = false;
-            });
-            $element.on('focus', function (e) {
-                //that.isOpen.status = !that.isOpen.status;
-                that.isOpen = true;
-            });*/
-
-            this.toggleOpen = function () {
-
-                if (that.controlDisabled === 'true') {
-                    this.isOpen.status = false;
-                    return false;
-                }
-                this.isOpen = !this.isOpen;
-            };
-
-            this.selectValue = function (event, member) {
-
-                if (oldMemberId === member.id) {
-                    return true;
-                }
-
-                if (member.id === 'root') {
-                    member.name = event.currentTarget.innerText;
-                }
-                //that.currentMember = member;
-                $scope.changeEvent(member);
-                that.currentMember = member;
-                oldMemberId = member.id;
-
-            };
-        };
-
         return {
             restrict: 'E',
-            controller: controller,
+            controller: 'NestedComboboxController',
             controllerAs: 'gs',
             replace: true,
-            templateUrl: '../src/select-group.html',
+            templateUrl: 'template/select-group.html',
             scope: {
                 collection: '=',
                 currentMember: '=',
